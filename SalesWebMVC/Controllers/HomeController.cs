57214fc307deaf2234319a446f8615bc0d1186ad
Igor Merten <igormeerten@gmail.com>
2019-10-04 06:22:07 +0100

    Primeiro Model-View-Controller: Department

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
@@ -17,7 +18,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# course.";
 
             return View();
         }
